12232b2b222ffc2ec4d8e9da6144a8bd606e099d
Igor Frolov <adromaryn@gmail.com>
2015-12-09 21:46:37 +0300

    google oauth2 fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"],
     {
-    :client_options => { :ssl => { :ca_file => 'C:\Ruby21\cacert.pem'}},
-    :provider_ignores_state => true,
     :redirect_uri => 'https://adservice-adromaryn/users/auth/google_oauth2/callback',
     :prompt => "select_account",
     setup: (lambda do |env|
