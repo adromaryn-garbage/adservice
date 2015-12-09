383b4f38ae3cf68f2a3c68ce8d0f8ad7c5972c0a
Igor Frolov <adromaryn@gmail.com>
2015-12-09 21:41:20 +0300

    google oauth2 fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
-      :name => "google",
-      :scope => "email, profile, plus.me, http://gdata.youtube.com",
-      :prompt => "select_account",
-      :image_aspect_ratio => "square",
-      :image_size => 50
-      setup: (lambda do |env|
-        request = Rack::Request.new(env)
-        env['omniauth.strategy'].options['token_params'] = {:redirect_uri => 'https://.../users/auth/google_oauth2/callback'}
-      end)
+    :client_options => { :ssl => { :ca_file => 'C:\Ruby21\cacert.pem'}},
+    :provider_ignores_state => true,
+    :redirect_uri => 'https://.../users/auth/google_oauth2/callback',
+    :prompt => "select_account",
+    setup: (lambda do |env|
+      request = Rack::Request.new(env)
+      env['omniauth.strategy'].options['token_params'] = {:redirect_uri => 'https://.../users/auth/google_oauth2/callback'}
+    end)
     }
 end
