9535c45aed4eed38babce5c6ecca79917d9e8ef2
Igor Frolov <adromaryn@gmail.com>
2015-12-09 21:49:09 +0300

    google oauth2 fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"],
     {
-    :redirect_uri => 'https://adservice-adromaryn/users/auth/google_oauth2/callback',
+    :redirect_uri => 'https://adservice-adromaryn.herokuapp.com/users/auth/google_oauth2/callback',
     :prompt => "select_account",
     setup: (lambda do |env|
       request = Rack::Request.new(env)
-      env['omniauth.strategy'].options['token_params'] = {:redirect_uri => 'https://adservice-adromaryn/users/auth/google_oauth2/callback'}
+      env['omniauth.strategy'].options['token_params'] = {:redirect_uri => 'https://adservice-adromaryn.herokuapp.com/users/auth/google_oauth2/callback'}
     end)
     }
 end
